f8b3266bf6cf0def7c7e24252b060ac9603a555b
Daneatriz <danielcanhamaque@gmail.com>
2024-04-04 18:24:22 -0300

    teste commit

--- app/src/main/java/beatriz/daniel/email/MainActivity.java
+++ app/src/main/java/beatriz/daniel/email/MainActivity.java
@@ -10,14 +10,17 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
         Button btnEnviar = (Button) findViewById(R. id. btnEnviar);
         //Definição da ação do click do botão
         btnEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
-        public void onClick(View v){
+            public void onClick(View v) {
+
+            }
         });
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
     }
 }
