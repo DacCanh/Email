ba1621fab620b668519b18a08821ef32e6033491
Daneatriz <danielcanhamaque@gmail.com>
2024-04-09 14:23:12 -0300

    finalizou

--- app/src/main/java/beatriz/daniel/email/MainActivity.java
+++ app/src/main/java/beatriz/daniel/email/MainActivity.java
@@ -24,29 +24,29 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //Obtendo dados digitados pelo usuario
-                EditText etEmail = (EditText) findViewById(R. id.etEmail);
-                String email = etEmail.getText().toString();
+                EditText etEmail = (EditText) findViewById(R. id.etEmail); //obtencao do campo EditText de id etEmail
+                String email = etEmail.getText().toString(); //obter o email digitado pelo usuário
 
-                EditText etAssunto = (EditText) findViewById(R. id.etAssunto);
-                String assunto = etAssunto.getText().toString();
+                EditText etAssunto = (EditText) findViewById(R. id.etAssunto); //obtencao do campo EditText de id etAssunto
+                String assunto = etAssunto.getText().toString(); //obter o assunto digitado pelo usuário
 
-                EditText etTexto = (EditText) findViewById(R. id.etTexto);
-                String texto = etTexto.getText().toString();
+                EditText etTexto = (EditText) findViewById(R. id.etTexto); //obtencao do campo EditText de id etTexto
+                String texto = etTexto.getText().toString(); //obter o texto digitado pelo usuário
 
-                Intent i = new Intent(Intent.ACTION_SENDTO);
+                Intent i = new Intent(Intent.ACTION_SENDTO); //indicação da processo que faz o Android procurar as apps intaladas capazes de resolver a ação "ACTION SENDTO"
 
-                i.setData(Uri.parse("mailto:"));
+                i.setData(Uri.parse("mailto:")); //definindo o interesse somente em apps capazes de resolver a URI "mailto". URI = indicar quais apps trabalham com envio e recebiment de email
 
-                String[] emails = new String[] {email};
-                i.putExtra(Intent.EXTRA_EMAIL, emails);
-                i.putExtra(Intent.EXTRA_SUBJECT, assunto);
-                i.putExtra(Intent.EXTRA_TEXT, texto);
+                String[] emails = new String[] {email}; //preenchendo o Intent com os dados que devem ser enviados para a app externa e que, posteriormente, enviará para o email
+                i.putExtra(Intent.EXTRA_EMAIL, emails); //lista de nomes(strings) correspondente ao email de cada destinatário
+                i.putExtra(Intent.EXTRA_SUBJECT, assunto); //campo de assunto
+                i.putExtra(Intent.EXTRA_TEXT, texto); //corpo de texto do email
 
                 try {
-                    startActivity(Intent.createChooser(i, "Escolha o APP"));
+                    startActivity(Intent.createChooser(i, "Escolha o APP")); //execução da Intent. Intent.createChooser = comando que dá a opção ao usuário de escolher as apps para enviar uma mensagem de email
                 }
                 catch (ActivityNotFoundException e) {
-                    Toast.makeText(MainActivity.this, "Não há nenhum app que posso realizar essa operação", Toast.LENGTH_LONG).show();
+                    Toast.makeText(MainActivity.this, "Não há nenhum app que posso realizar essa operação", Toast.LENGTH_LONG).show();//mensagem que aparecerá caso não há uma app capaz de enviar uma mensagem de email.
                 }
 
 
